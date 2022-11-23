4924a4f1e7e344c92d1f0ac595a75bd12d95ed7c
ewerton11 <ewertonreisc0@gmail.com>
2022-11-23 17:09:08 -0300

    config babel

---
+++ my-app/src/pages/home/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
