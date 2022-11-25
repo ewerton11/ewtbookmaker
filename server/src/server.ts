a64be62bce8aea1eeda80b71d2a7e91cfcf520d4
ewerton11 <ewertonreisc01@gmail.com>
2022-11-25 10:52:11 -0300

    server

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
