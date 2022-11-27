2af69bcf88dcaba2724b2acb73a126ef58032a55
ewerton11 <ewertonreisc01@gmail.com>
2022-11-26 21:27:54 -0300

    modificaçao no estilo login e conexao com banco de dados

--- web/src/styles/pages/login/style.ts
+++ web/src/styles/pages/login/style.ts
@@ -13,42 +13,24 @@ export const Page = styled.div`
   align-items: center;
 `
 
-export const Container = styled.div`
+export const Container = styled.form`
   width: 500px;
   height: 600px;
   background-color: #292929;
   border: 1px solid white;
   border-radius: 50px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-export const ContainerDiv = styled.div`
-  width: 100%;
-  height: 50%;
-  margin-top: 10%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-`
-
-export const ContainerInputEmail = styled.div`
-  width: 90%;
-  height: 30%;
-  background-color: white;
-  border-radius: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: relative;
 `
 
 export const ContainerInputUser = styled.div`
   width: 90%;
-  height: 30%;
+  height: 20%;
   background-color: white;
   border-radius: 50px;
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translateX(-50%);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -65,28 +47,18 @@ export const Buttom = styled.button`
   width: 90%;
   height: 20%;
   border-radius: 50px;
-  position: relative;
-  margin-top: 10%;
+  background-color: blue;
+  border-radius: 50px;
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translateX(-50%);
+  font-family: "Roboto", sans-serif;
+  font-size: 2rem;
+  color: white;
+  transition: ease 0.3s;
 
-  & a {
-    width: 100%;
-    height: 100%;
-    background-color: blue;
-    border-radius: 50px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-family: "Roboto", sans-serif;
-    font-size: 2rem;
-    color: white;
-    transition: ease 0.3s;
-  }
-
-  & a:hover {
+  &:hover {
     background-color: rgb(77, 77, 255);
   }
 `
