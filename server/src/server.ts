bbb27f4cfe77074e49b6193ae8a6b4a865a90a8c
ewerton11 <ewertonreisc0@gmail.com>
2022-11-26 21:28:47 -0300

    configuraçao back end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,21 @@
 import Fastify from "fastify"
+// import cors from "@fastify/cors"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient({
   log: ["query"],
 })
 
-const fastify = Fastify({
-  logger: true,
-})
-
 async function start() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  fastify.register(require("@fastify/cors"), {
+    origin: ["http://localhost:3001"],
+    methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
+  })
+
   fastify.post("/user", async (request, reply) => {
     const { name } = request.body
 
