a632d5b4863fc8c734fa37488de94b485db4bfc1
ewerton11 <ewertonreisc0@gmail.com>
2022-11-30 20:37:37 -0300

    busca de dados do bando de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ async function start() {
   })
 
   fastify.register(require("@fastify/cors"), {
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
   })
 
@@ -27,7 +27,27 @@ async function start() {
     return reply.status(201).send({ name })
   })
 
-  fastify.listen({ port: 3000 }, function (err) {
+  fastify.post("/bets", async (request, reply) => {
+    const { title, value, description }: any = request.body
+
+    await prisma.bets.create({
+      data: {
+        title,
+        value,
+        description,
+      },
+    })
+
+    return reply.status(201).send({ title, value, description })
+  })
+
+  fastify.get("/bets", async (request, reply) => {
+    const data = await prisma.bets.findMany()
+
+    return reply.status(200).send(data)
+  })
+
+  fastify.listen({ port: 3333 }, function (err) {
     if (err) {
       fastify.log.error(err)
       process.exit(1)
