da32f0b4f3e389047a0ce4772e5ea07100f2efec
ewerton11 <ewertonreisc0@gmail.com>
2022-12-03 23:30:32 -0300

    fonte

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -12,12 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
             rel="stylesheet"
-          ></link>
+          />
         </Head>
       </div>
 
