c013e6d4e0a82e91e1b7a8ca4791c1fa70fb153e
ewerton11 <ewertonreisc0@gmail.com>
2022-12-04 21:40:17 -0300

    estilo bets

--- web/src/styles/pages/bets/headerBets/style.ts
+++ web/src/styles/pages/bets/headerBets/style.ts
@@ -16,6 +16,7 @@ export const Header = styled.header`
 export const ContainerSearch = styled.div`
   width: 100%;
   height: 10%;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -48,8 +49,8 @@ export const ContainerBets = styled.div`
 export const PreviewBets = styled.div`
   width: 100%;
   height: 20%;
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  margin: 5% 0px;
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
   display: flex;
   flex-direction: column;
   justify-content: space-around;
