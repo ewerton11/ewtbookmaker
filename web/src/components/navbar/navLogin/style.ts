37011b04cb7e6a4d4cea1fd4fcab5099d2eb14c0
ewerton11 <ewertonreisc0@gmail.com>
2022-12-07 20:46:50 -0300

    finalizaçao bets

--- web/src/components/navbar/navLogin/style.ts
+++ web/src/components/navbar/navLogin/style.ts
@@ -1,19 +1,16 @@
 import styled from "styled-components"
 
 export const ContainerLogin = styled.div`
-  width: 100%;
-  height: 40%;
-  background-color: aliceblue;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  width: 13%;
+  height: 100%;
   display: flex;
-  justify-content: end;
+  justify-content: space-around;
   align-items: center;
 `
 
 export const DivRegister = styled.div`
-  width: 5%;
-  height: 90%;
-  margin-right: 1%;
+  width: 50%;
+  height: 70%;
   cursor: pointer;
   transition: ease 0.3s;
 
@@ -34,9 +31,8 @@ export const Register = styled.div`
 `
 
 export const DivLogin = styled.div`
-  width: 5%;
-  height: 90%;
-  margin-right: 1%;
+  width: 40%;
+  height: 70%;
   background-color: rgb(77, 77, 255);
   border-radius: 5px;
   cursor: pointer;
