bcaae5b9565c7cdba9b74dd4bf8d06e6cc63ecf6
ewerton11 <ewertonreisc01@gmail.com>
2022-12-11 15:21:05 -0300

    estilizaçao login

---
+++ web/src/styles/pages/login/register/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const ContainerRegister = styled.div`
+  width: 90%;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
